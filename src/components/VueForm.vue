70c85ea32e7db524825ae8144558d084594b79f2
IlScottlI <74098716+IlScottlI@users.noreply.github.com>
2023-08-13 08:53:32 -0400

    Form from json

---
+++ src/components/VueForm.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+#app.section
+        form-json(
+            :btnReset="{ value: 'Reset' }",
+            :btnSubmit="{ value: 'Submit' }",
+            :camelizePayloadKeys="true",
+            :formFields="jsonFields",
+            formName="userProfil"
+        )
+</template>
+    
+<script>
+// import 'bulma/css/bulma.min.css'
+import '@/form.css'
+import formJson from "vue-form-json";
+import jsonFields from "@/assets/fields";
+
+export default {
+    name: "app",
+    components: {
+        formJson,
+    },
+    mounted() {
+        this.$root.$on("formSubmitted", (values) => alert(JSON.stringify(values)));
+    },
+    computed: {
+        jsonFields: () => jsonFields,
+    },
+};
+</script>
+    
